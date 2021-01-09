a2530e634f646c6e0100f5e71c610e2241afdd08
Boligude <gui.resende@hotmail.com>
2021-01-08 21:45:17 -0300

    Implementação de aparecimento e desaparecimento aleatorio de mosca assim como lado aleatorio e background e inicio do tempo e vida

--- cods/jogo.js
+++ cods/jogo.js
@@ -10,6 +10,12 @@ ajusteTamanhoTela()
 
 
 function posicaoRandomica() {
+
+    //remover a mosca anterior (caso exista)
+    if(document.getElementById('mosca')){
+    document.getElementById('mosca').remove()
+    }
+
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -21,9 +27,42 @@ function posicaoRandomica() {
     //criar o elemento Mosca
     var mosca = document.createElement('img')
     mosca.src = "../imagens/mosca.png"
-    mosca.className = 'mosquito1'
+    mosca.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
     mosca.style.left = posicaoX + 'px'
     mosca.style.top = posicaoY + 'px'
     mosca.style.position = 'absolute'
+    mosca.id='mosca'
+
     document.body.appendChild(mosca)
+
+
+
+}
+
+function tamanhoAleatorio() {
+    var classe =  Math.floor(Math.random() * 3)
+    
+    switch(classe) {
+        case 0:
+            return 'mosquito1'
+
+        case 1:
+            return 'mosquito2'
+
+        case 2:
+            return 'mosquito3'
+    }
+}
+
+function ladoAleatorio() {
+    var classe =  Math.floor(Math.random() * 2)
+    
+    switch(classe) {
+        case 0:
+            return 'ladoA'
+
+        case 1:
+            return 'ladoB'
+
+    }
 }
